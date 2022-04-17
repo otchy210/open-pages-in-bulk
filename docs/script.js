6e3f19d767fc0f7a15308062a0d6d60dfd0b6c48
Otchy <id+github@otchy.net>
2022-04-17 13:52:28 +0900

    Implement open-then-next feature

--- docs/script.js
+++ docs/script.js
@@ -13,16 +13,34 @@ const parseUrlParams = () => {
 const newEl = (tagName, attrs = {}, innerText = '') => {
     const el = document.createElement(tagName);
     Object.entries(attrs).forEach(([name, value]) => {
-        el.setAttribute(name, value);
+        if (value) {
+            el.setAttribute(name, value);
+        }
     })
     el.innerText = innerText;
     return el;
 }
 
-const buildLi = (site) => {
+const getCheckboxes = () => {
+    return Array.from(document.querySelectorAll('input.site'));
+}
+
+const selectSite = (selected) => {
+    selected.checked = true;
+    getCheckboxes().forEach((cb) => {
+        cb.checked = (cb === selected);
+    })
+}
+
+const onChangeSite = (e) => {
+    selectSite(e.target);
+}
+
+const buildLi = (site, checked) => {
     const li = newEl('li');
     const label = newEl('label');
-    const cb = newEl('input', {type: 'checkbox', class: 'site', value: site.url, 'checked': 'checked'});
+    const cb = newEl('input', {type: 'checkbox', class: 'site', value: site.url, checked: checked ? 'checked' : null});
+    cb.addEventListener('change', onChangeSite);
     const title = newEl('span', {}, site.title);
     label.appendChild(cb);
     label.appendChild(title);
@@ -30,27 +48,25 @@ const buildLi = (site) => {
     return li;
 }
 
-const openNewTab = (url) => {
-    window.open(url, '_blank');
-    // const a = newEl('a', {href: url, target: '_blank'});
-    // a.click();
-}
-
 const rootEl = document.body.querySelector('#root');
 const sites = parseUrlParams();
 const list = newEl('ul');
-sites.forEach((site) => {
-    const li = buildLi(site);
+sites.forEach((site, i) => {
+    const li = buildLi(site, i === 0);
     list.appendChild(li);
 });
 rootEl.appendChild(list);
 
-const button = newEl('div', {class: 'button'}, 'まとめて開く');
+const button = newEl('div', {class: 'button'}, '開いて次へ');
 button.addEventListener('click', () => {
-    const checkedUrl = Array.from(document.querySelectorAll('input.site:checked')).map(input => input.value);
-    checkedUrl.forEach((url) => {
-        openNewTab(url);
-    });
+    const checkboxes = getCheckboxes();
+    const currentIndex = checkboxes
+        .map((cb, index) => {return {checked: cb.checked, index}} )
+        .filter((cb) => cb.checked)[0].index;
+    const nextIndex = (currentIndex + 1) % checkboxes.length;
+    selectSite(checkboxes[nextIndex]);
+
+    window.open(checkboxes[currentIndex].value, '_blank');
 });
 const p = newEl('p');
 p.appendChild(button);
